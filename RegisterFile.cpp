ea3b25813c896cdbc736e0c5e6b6391021a8b364
Alexander Mot <asd777101@gmail.com>
2020-08-05 20:17:11 -0700

    Fix an oopsie

--- RegisterFile.cpp
+++ RegisterFile.cpp
@@ -4,4 +4,4 @@ RegisterFile::RegisterFile(){registers.reserve(16);};
 RegisterFile::~RegisterFile(){};
 
 int RegisterFile::getRegValue(int pos){return registers[pos].value;};
-int RegisterFile::getRegValidity(int pos){return registers[pos].valid;};
+bool RegisterFile::getRegValidity(int pos){return registers[pos].valid;};
