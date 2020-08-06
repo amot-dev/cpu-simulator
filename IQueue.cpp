7c0c5455eb9db6dc22820409b398cd43c8923d28
Alexander Mot <asd777101@gmail.com>
2020-08-05 19:41:18 -0700

    Add functions to load and unload queues

---
+++ IQueue.cpp
@@ -0,0 +1,25 @@
+#include "IQueue.h"
+
+IQueue::IQueue(){};
+IQueue::~IQueue(){};
+
+void IQueue::loadIQueue(short opCode, int dest, bool valid1, int src1, bool valid2, int src2){
+	operation.push(opCode);								//push all values given to the various queues
+	destination.push(dest);
+	validA.push(valid1);
+	sourceA.push(src1);
+	validB.push(valid2);
+	sourceB.push(src2);
+	if (ROB_ID.size()) ROB_ID.push(ROB_ID.back() + 1);	//if this is not the first entry, set the ROB_ID to 1 greater than the previous ROB_ID
+	else ROB_ID.push(0);								//if it is the first entry, set the ROB_ID to 0
+};
+
+void IQueue::unloadIQueue(){
+	operation.pop();		//pop oldest value from each queue
+	destination.pop();
+	validA.pop();
+	sourceA.pop();
+	validB.pop();
+	sourceB.pop();
+	ROB_ID.pop();
+};
--- IQueue.h
+++ IQueue.h
@@ -1,6 +1,25 @@
 #ifndef IQUEUE_H
 #define IQUEUE_H
 
-class IQueue{};
+#include <string>
+#include <queue>
+
+class IQueue{
+public:
+	IQueue();
+	~IQueue();
+
+	void loadIQueue(short, int, bool, int, bool, int);	//pushes a new set of values to the instruction queue
+	void unloadIQueue();								//pops the oldest set of values from the instruction queue
+
+private:
+	std::queue<short> operation;
+	std::queue<int> destination;
+	std::queue<bool> validA;
+	std::queue<int> sourceA;
+	std::queue<bool> validB;
+	std::queue<int> sourceB;
+	std::queue<short> ROB_ID;
+};
 
 #endif
